2d96acc8ddf0668d2201b9408b6b0f67bf91637c
Kyosukke <kyosukke1@gmail.com>
2015-10-03 23:54:26 +0200

    Dataset Added

---
+++ src/utils/ImageDisplayer.java
@@ -0,0 +1,118 @@
+package utils;
+
+import java.awt.FlowLayout;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Mat;
+
+//import sun.misc.BASE64Encoder;
+
+/*
+ * Display image for debugger
+ */
+public class ImageDisplayer {
+/*	public static String encoreToString(BufferedImage image, String type){
+    	String imageString = null;
+    	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    	
+    	try {
+    		ImageIO.write(image, type, bos);
+    		byte[] imageBytes = bos.toByteArray();
+    		
+    		BASE64Encoder encoder = new BASE64Encoder();
+    		imageString = encoder.encode(imageBytes);
+    		bos.close();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return imageString;
+	}*/
+
+	public static BufferedImage Mat2BufferedImage(Mat m) {
+		int type = BufferedImage.TYPE_BYTE_GRAY;
+		
+		if (m.channels() > 1) {
+			type = BufferedImage.TYPE_3BYTE_BGR;
+		}
+		
+		int buffer_size = m.channels() * m.cols() * m.rows();
+		
+		byte[] b = new byte[buffer_size];
+		
+		m.get(0, 0, b);
+		
+		BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+		
+		final byte[] target_pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+		System.arraycopy(b, 0, target_pixels, 0, b.length);
+		
+		return image;
+	}
+	
+	public static BufferedImage resizeImage(String imagePath, String ext, int maxX, int maxY) {
+
+		BufferedImage originalImage;
+		try {
+			originalImage = ImageIO.read(new File(imagePath));
+			
+			if (originalImage.getWidth(null) < maxX && originalImage.getHeight(null) < maxY) {
+				return originalImage;
+			}
+			int reduction = 2;
+			while ((originalImage.getWidth(null) / reduction) > maxX || (originalImage.getHeight(null) / reduction) > maxY)
+				reduction *= 2;
+			
+			BufferedImage resizedImage = new BufferedImage((originalImage.getWidth(null) + 50) / reduction, (originalImage.getHeight(null) + 50) / reduction, BufferedImage.TYPE_INT_RGB);
+			Graphics2D graphic = (Graphics2D) resizedImage.createGraphics();
+			
+			graphic.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
+			graphic.drawImage(originalImage, 0, 0, (originalImage.getWidth(null) + 50) / reduction, (originalImage.getHeight(null) + 50) / reduction, null);
+
+			ImageIO.write(resizedImage, ext, new File(imagePath));
+
+			return resizedImage;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	public static void saveImage(String imageName, String imagePath, String ext, byte[] imageBytes) {
+		BufferedImage image;
+		try {
+			InputStream in = new ByteArrayInputStream(imageBytes);
+			image = ImageIO.read(in);
+			ImageIO.write(image, ext, new File(imagePath + imageName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void displayImage(Image img, String title) {
+		
+		JFrame frame = new JFrame();
+		frame.setLayout(new FlowLayout());
+		frame.setSize((img.getWidth(null) + 50) / 1, (img.getHeight(null) + 50) / 1);
+
+		JLabel lbl = new JLabel(new ImageIcon((Image)img));
+//		lbl.setIcon(icon);
+		frame.add(lbl);
+		frame.setVisible(true);
+		frame.setTitle(title);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
--- src/utils/Initializer.java
+++ src/utils/Initializer.java
@@ -1,12 +1,19 @@
+package utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class Initializer {
+	
+	static String path = "assets/step1/";
 	
 	public static void main(String[] args) {
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
 	    Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
 	    System.out.println( "mat = " + mat.dump() );
+	    
+	    Mat out = Imgcodecs.imread(path + "a.bmp", Imgcodecs.CV_LOAD_IMAGE_COLOR);
+	    ImageDisplayer.displayImage(ImageDisplayer.Mat2BufferedImage(out), "test");
 	}
 }
