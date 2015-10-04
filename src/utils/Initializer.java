6599ef29bc335952e34c6f78b28c30b23f3041f9
Kyosukke <kyosukke1@gmail.com>
2015-10-04 21:21:25 +0200

    KNN Algorithm implemented. To Test

--- src/utils/Initializer.java
+++ src/utils/Initializer.java
@@ -1,10 +1,12 @@
 package utils;
+import java.util.List;
+
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
 import charrecognition.CharacterRecognition;
 import preprocessing.ImageCleaner;
+import utils.Character;
 
 public class Initializer {
 	
@@ -12,9 +14,12 @@ public class Initializer {
 	
 	public static void main(String[] args) {
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-	    
-	    Mat out = ImageCleaner.CleanImage(path + "m.bmp");
-	    System.out.println("Text Found: " + CharacterRecognition.getCharacter(out));
+		System.out.println(Core.NATIVE_LIBRARY_NAME);
+		List<Character> dataset;
+
+		Mat out = ImageCleaner.CleanImage(path + "j.bmp");
+	   	dataset = DatasetManager.getAlphaNumericFrom(path, ".bmp");
+	    System.out.println("Text Found: " + CharacterRecognition.getCharacter(out, dataset));
 	    ImageDisplayer.displayImage(ImageDisplayer.Mat2BufferedImage(out), "test");
 	}
 }
